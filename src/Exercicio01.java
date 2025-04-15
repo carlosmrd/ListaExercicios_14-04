0ff3ad487a2ba65e06fb6434ba0976a069518f8d
Carlos <carlosmrd2001@gmail.com>
2025-04-14 21:37:26 -0300

    Update Exercicios

--- src/Exercicio01.java
+++ src/Exercicio01.java
@@ -7,11 +7,24 @@ public class Exercicio01 {
         //determinado dia. Para cada casa consultada foi fornecido o número do canal (4, 5, 7, 12) e o
         //número de pessoas que estavam assistindo aquele canal. Se a televisão estivesse desligada, nada
         //era anotado, ou seja, essa casa não entrava na pesquisa. Faça um programa que:
-        //• leia um número indeterminado de dados (número do canal e número de pessoas que estavam assistindo); e
+        //• leia um número indeterminado de dados (número do canal e número de pessoas que estavam assistindo); e
         //• calcule e mostre a porcentagem de audiência de cada canal.
         //Para encerrar a entrada de dados, digite o número do canal ZERO.
 
         System.out.println("Teste");
+
+        int channel = 0;
+        int viewers;
+
+        do {
+            System.out.print("Insira o número do canal, ou 0 para encerrar: ");
+            channel = scanner.nextInt();
+
+            System.out.print("Insira a quantidade de pessoas da casa: ");
+            viewers = scanner.nextInt();
+
+
+        } while (channel != 0);
 
     }
 }
--- src/Exercicio02.java
+++ src/Exercicio02.java
@@ -14,7 +14,75 @@ public class Exercicio02 {
         //Na opção 3: sair do programa.
         //Verifique a possibilidade de opção inválida. Nesse caso, o programa deverá mostrar uma mensagem.
 
-        System.out.println("Teste 2");
+        int option;
+
+        do {
+            System.out.println("Menu de opções:");
+            System.out.println("\t1. Média aritmética");
+            System.out.println("\t2. Média ponderada");
+            System.out.println("\t3. Sair");
+            System.out.print("Selecione uma opção: ");
+            option = scanner.nextInt();
+            System.out.println();
+
+
+            double firstScore;
+            double firstWeight;
+            double secondScore;
+            double secondWeight;
+            double thirdScore;
+            double thirdWeight;
+            double result;
+
+            switch (option) {
+                case 1: {
+                    System.out.println("Média aritmética:\n");
+
+                    System.out.print("Insira a primeira nota: ");
+                    firstScore = scanner.nextDouble();
+
+                    System.out.print("Insira a segunda nota: ");
+                    secondScore = scanner.nextDouble();
+
+                    result = (firstScore + secondScore) / 2;
+
+                    System.out.printf("A média aritmética é %.2f\n", result);
+                }
+                break;
+                case 2: {
+                    System.out.println("Média ponderada:\n");
+
+                    System.out.print("Insira a primeira nota: ");
+                    firstScore = scanner.nextDouble();
+
+                    System.out.print("Insira o peso da primeira nota: ");
+                    firstWeight = scanner.nextDouble();
+
+                    System.out.print("Insira a segunda nota: ");
+                    secondScore = scanner.nextDouble();
+
+                    System.out.print("Insira o peso da segunda nota: ");
+                    secondWeight = scanner.nextDouble();
+
+                    System.out.print("Insira a terceira nota: ");
+                    thirdScore = scanner.nextDouble();
+
+                    System.out.print("Insira o peso da terceira nota: ");
+                    thirdWeight = scanner.nextDouble();
+
+                    result = ((firstScore * firstWeight) + (secondScore * secondWeight) + (thirdScore * thirdWeight)) / (firstWeight + secondWeight + thirdWeight);
+
+                    System.out.printf("A média ponderada é %.2f\n", result);                }
+                break;
+                case 3: {
+                    System.out.println("Encerrando o programa.");
+                }
+                break;
+                default:
+                    System.out.println("Opção inválida, insira novamente.");
+            }
+            System.out.println();
+        } while (option != 3);
 
     }
 }
